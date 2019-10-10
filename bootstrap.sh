61eaea1feaa742721162f9caa0a76a4b9661c21f
Justin Güse <guese.justin@gmail.com>
2019-10-10 12:47:50 +0200

    fixing errors in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source .env
+
 # S3_BUCKET=guesonex-databackup  # < shoud work with source .env
 # git setup
 yum install git -y
@@ -11,10 +11,11 @@ systemctl enable docker.service
 systemctl start docker.service
 # docker compose install
 yum install -y python-pip
-yum install -y python-pip
+yum install -y docker-compose
 yum upgrade python* -y
 git clone https://github.com/JustinGuese/wordpress-docker-compose.git
 cd wordpress-docker-compose
+source .env
 # used to pull current files into machine before starting docker - set up ec2 IAM roles or ~/.aws/ credentials if hosti
 # pulling wp-content
 aws s3 sync s3://$S3_BUCKET/gusonex-wp/wp-content/ ./wp-app/wp-content/
